90118e82380c18eb2a1d61c4098be6d9c663d064
AbrilAbad <164096598+AbrilAbad@users.noreply.github.com>
2025-10-05 20:55:43 -0300

    Confeccion de formulario

--- Admision/AdmitirEncomienda.Designer.cs
+++ Admision/AdmitirEncomienda.Designer.cs
@@ -28,18 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            columnHeader8 = new ColumnHeader();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(23, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 37);
+            button1.TabIndex = 0;
+            button1.Text = "Buscar Guía";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(23, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(289, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Número de Guía";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Detalle Guía";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader6, columnHeader7, columnHeader8 });
+            listView1.Location = new Point(23, 202);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(935, 121);
+            listView1.TabIndex = 4;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "N° Guía";
+            columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Tipo";
+            columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "ID Cliente";
+            columnHeader3.Width = 150;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Nombre Destinatario";
+            columnHeader6.Width = 160;
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "Dirección Destinatario";
+            columnHeader7.Width = 160;
+            // 
+            // columnHeader8
+            // 
+            columnHeader8.Text = "Tel. Destinatario";
+            columnHeader8.Width = 160;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(685, 355);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 37);
+            button2.TabIndex = 5;
+            button2.Text = "Admitir";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(834, 355);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 37);
+            button3.TabIndex = 6;
+            button3.Text = "Rechazar";
+            button3.UseVisualStyleBackColor = true;
             // 
             // AdmitirEncomienda
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1003, 431);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(listView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "AdmitirEncomienda";
             Text = "AdmitirEncomienda";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private Button button2;
+        private Button button3;
     }
 }
