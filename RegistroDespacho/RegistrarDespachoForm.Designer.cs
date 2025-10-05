b7ff200bc0cdf4f6132864b3e54b295ac29367ec
Renata <renatubio@gmail.com>
2025-10-05 16:21:43 -0300

    despachoForm

--- RegistroDespacho/RegistrarDespachoForm.Designer.cs
+++ RegistroDespacho/RegistrarDespachoForm.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "RegistrarDespachoForm";
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            groupBox1 = new GroupBox();
+            checkedListBox1 = new CheckedListBox();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Selección de empresa";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(197, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Selección de transporte";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(265, 42);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(327, 33);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(265, 105);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(327, 33);
+            comboBox2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkedListBox1);
+            groupBox1.Location = new Point(57, 158);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(535, 204);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Paquetes a cargar:";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(20, 52);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(487, 116);
+            checkedListBox1.TabIndex = 0;
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(453, 395);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 34);
+            button1.TabIndex = 5;
+            button1.Text = "Finalizar carga";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // RegistrarDespachoForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(637, 450);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "RegistrarDespachoForm";
+            Text = "RegistrarDespachoForm";
+            Load += RegistrarDespachoForm_Load;
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private GroupBox groupBox1;
+        private Button button1;
+        private CheckedListBox checkedListBox1;
     }
 }
--- RegistroDespacho/RegistrarDespachoForm.cs
+++ RegistroDespacho/RegistrarDespachoForm.cs
@@ -16,5 +16,35 @@ namespace CAIGrupoG.RegistroDespacho
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add("Paquete A");
+            checkedListBox1.Items.Add("Paquete B");
+            checkedListBox1.Items.Add("Paquete C");
+            checkedListBox1.Items.Add("Paquete D");
+
+            this.Controls.Add(checkedListBox1);
+        }
+
+        private void RegistrarDespachoForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
