829f4ce7731f9a53e8b701875bdb97c40f9186b4
Ivan Otero <Ivan Otero@DESKTOP-9OPP9JT>
2025-10-05 22:08:22 -0300

    Ajuste inicial pantalla rendición.

--- RendicionHDR/CargarRendicionHDRForm.Designer.cs
+++ RendicionHDR/CargarRendicionHDRForm.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CargarRendicionHDRForm";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            checkedListBox1 = new CheckedListBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 15);
+            label1.TabIndex = 0;
+            label1.Text = "DNI del transportista";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(308, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(327, 31);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Guias asociadas";
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(12, 79);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(390, 148);
+            checkedListBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 233);
+            button2.Name = "button2";
+            button2.Size = new Size(187, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Marcar como entregadas";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(215, 233);
+            button3.Name = "button3";
+            button3.Size = new Size(187, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Cancelar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(113, 270);
+            button4.Name = "button4";
+            button4.Size = new Size(187, 23);
+            button4.TabIndex = 7;
+            button4.Text = "Generar resumen";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // CargarRendicionHDRForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(419, 305);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(checkedListBox1);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "CargarRendicionHDRForm";
+            Text = "CargarRendicionHDRForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private CheckedListBox checkedListBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
