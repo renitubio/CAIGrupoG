5787f82df8189524d7e8c0439949dd5944c73739
agustin dezza <agusdezza@hotmail.com>
2025-10-06 11:38:00 -0300

    Emitir Factura Corregido (Agus)

--- EmitirFactura/EmitirFactura.Designer.cs
+++ EmitirFactura/EmitirFactura.Designer.cs
@@ -28,210 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            label1 = new Label();
             dataGridView1 = new DataGridView();
             Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewTextBoxColumn();
             Column3 = new DataGridViewTextBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             Column5 = new DataGridViewTextBoxColumn();
-            Column6 = new DataGridViewComboBoxColumn();
-            Column7 = new DataGridViewTextBoxColumn();
-            Column8 = new DataGridViewTextBoxColumn();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            comboBox1 = new ComboBox();
-            comboBox2 = new ComboBox();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            Column6 = new DataGridViewTextBoxColumn();
             checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            button1.Location = new Point(701, 300);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Atrás";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(493, 300);
-            button2.Name = "button2";
-            button2.Size = new Size(202, 23);
-            button2.TabIndex = 1;
-            button2.Text = "Generar e Imprimir Comprobante";
-            button2.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(72, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Guías Rendidas";
             // 
             // dataGridView1
             // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonFace;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8 });
-            dataGridView1.Location = new Point(0, 147);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6 });
+            dataGridView1.Location = new Point(72, 83);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(799, 82);
+            dataGridView1.Size = new Size(643, 273);
             dataGridView1.TabIndex = 2;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // Column1
             // 
-            Column1.HeaderText = "Código";
+            Column1.HeaderText = "Seleción";
             Column1.Name = "Column1";
             // 
             // Column2
             // 
-            Column2.HeaderText = "Servicio";
+            Column2.HeaderText = "N° Guía Seguimiento";
             Column2.Name = "Column2";
             // 
             // Column3
             // 
-            Column3.HeaderText = "Cantidad";
+            Column3.HeaderText = "Fecha";
             Column3.Name = "Column3";
             // 
             // Column4
             // 
-            Column4.HeaderText = "Precio Unitario";
+            Column4.HeaderText = "CUIT/CUIL Cliente";
             Column4.Name = "Column4";
             // 
             // Column5
             // 
-            Column5.HeaderText = "Subtotal";
+            Column5.HeaderText = "Razón Social";
             Column5.Name = "Column5";
             // 
             // Column6
             // 
-            Column6.HeaderText = "Alícuota IVA";
+            Column6.HeaderText = "Monto a Facturar";
             Column6.Name = "Column6";
-            // 
-            // Column7
-            // 
-            Column7.HeaderText = "Importe IVA";
-            Column7.Name = "Column7";
-            // 
-            // Column8
-            // 
-            Column8.HeaderText = "Total";
-            Column8.Name = "Column8";
-            // 
-            // button3
-            // 
-            button3.Location = new Point(296, 247);
-            button3.Name = "button3";
-            button3.Size = new Size(202, 23);
-            button3.TabIndex = 3;
-            button3.Text = "Agregar línea descripción";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(12, 12);
-            button4.Name = "button4";
-            button4.Size = new Size(172, 23);
-            button4.TabIndex = 4;
-            button4.Text = "Fecha Comprobante";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(12, 41);
-            button5.Name = "button5";
-            button5.Size = new Size(172, 23);
-            button5.TabIndex = 5;
-            button5.Text = "Conceptos a Incluir";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(12, 70);
-            button6.Name = "button6";
-            button6.Size = new Size(172, 23);
-            button6.TabIndex = 6;
-            button6.Text = "Condición frente al IVA";
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            button7.Location = new Point(12, 99);
-            button7.Name = "button7";
-            button7.Size = new Size(172, 23);
-            button7.TabIndex = 7;
-            button7.Text = "CUIT/CUIL";
-            // 
-            // button8
-            // 
-            button8.Location = new Point(513, 12);
-            button8.Name = "button8";
-            button8.Size = new Size(170, 23);
-            button8.TabIndex = 8;
-            button8.Text = "Condiciones de Venta";
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(211, 41);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(153, 23);
-            comboBox1.TabIndex = 9;
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(211, 70);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(153, 23);
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(211, 12);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(153, 23);
-            textBox1.TabIndex = 11;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(211, 100);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(153, 23);
-            textBox2.TabIndex = 12;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(400, 43);
+            checkBox1.Location = new Point(82, 126);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(72, 19);
-            checkBox1.TabIndex = 13;
-            checkBox1.Text = "Contado";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 3;
             checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(683, 389);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Cancelar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(602, 389);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Emitir";
+            button2.UseVisualStyleBackColor = true;
             // 
             // EmitirFactura
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
             ClientSize = new Size(800, 450);
-            Controls.Add(checkBox1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
-            Controls.Add(button8);
-            Controls.Add(button7);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(dataGridView1);
             Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
             Name = "EmitirFactura";
             Text = "EmitirFactura";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -241,27 +139,16 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private Label label1;
         private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
-        private DataGridViewComboBoxColumn Column6;
-        private DataGridViewTextBoxColumn Column7;
-        private DataGridViewTextBoxColumn Column8;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private ComboBox comboBox1;
-        private ComboBox comboBox2;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private DataGridViewTextBoxColumn Column6;
         private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
     }
 }
--- EmitirFactura/EmitirFactura.cs
+++ EmitirFactura/EmitirFactura.cs
@@ -16,15 +16,5 @@ namespace CAIGrupoG.EmitirFactura
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
     }
 }
