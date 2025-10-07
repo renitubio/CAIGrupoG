3ed965a314b76aa300b8e3bee55e177b32494e5d
Renata <renatubio@gmail.com>
2025-10-07 13:54:00 -0300

    cambios en imposición

---
+++ Imposicion/ImpAgencia/ImposicionAgenciaForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAIGrupoG.Imposicion.ImpAgencia
+{
+    public partial class ImposicionAgenciaForm : Form
+    {
+        public ImposicionAgenciaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ImposicionAgenciaForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Imposicion/ImpCallCenter/ImposicionCallCenterForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAIGrupoG.Imposicion.ImpCallCenter
+{
+    public partial class ImposicionCallCenterForm : Form
+    {
+        public ImposicionCallCenterForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
