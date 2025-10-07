67cd66d456538e96a8be9a670bdf0ff43d326d41
Renata <renatubio@gmail.com>
2025-10-07 10:58:22 -0300

    cambios en imposición

--- Imposicion/ImponerEncomienda.Designer.cs
+++ Imposicion/ImponerEncomienda.Designer.cs
@@ -30,12 +30,10 @@
         {
             button1 = new Button();
             groupBox1 = new GroupBox();
-            textBox4 = new TextBox();
             textBox3 = new TextBox();
             textBox2 = new TextBox();
             textBox1 = new TextBox();
             label5 = new Label();
-            label4 = new Label();
             label3 = new Label();
             label2 = new Label();
             label1 = new Label();
@@ -45,10 +43,8 @@
             button3 = new Button();
             textBox10 = new TextBox();
             label16 = new Label();
-            comboBox2 = new ComboBox();
             textBox6 = new TextBox();
             textBox5 = new TextBox();
-            label11 = new Label();
             label10 = new Label();
             label9 = new Label();
             label8 = new Label();
@@ -61,100 +57,92 @@
             // 
             // button1
             // 
-            button1.Location = new Point(6, 224);
+            button1.Location = new Point(10, 212);
+            button1.Margin = new Padding(4, 4, 4, 4);
             button1.Name = "button1";
-            button1.Size = new Size(154, 51);
+            button1.Size = new Size(192, 64);
             button1.TabIndex = 0;
             button1.Text = "Buscar Cliente";
             button1.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(textBox4);
             groupBox1.Controls.Add(textBox3);
             groupBox1.Controls.Add(textBox2);
             groupBox1.Controls.Add(textBox1);
             groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(button1);
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(31, 71);
+            groupBox1.Location = new Point(39, 89);
+            groupBox1.Margin = new Padding(4, 4, 4, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(504, 281);
+            groupBox1.Padding = new Padding(4, 4, 4, 4);
+            groupBox1.Size = new Size(630, 290);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(176, 169);
-            textBox4.Name = "textBox4";
-            textBox4.ReadOnly = true;
-            textBox4.Size = new Size(126, 27);
-            textBox4.TabIndex = 10;
-            // 
             // textBox3
             // 
-            textBox3.Location = new Point(176, 137);
+            textBox3.Location = new Point(220, 171);
+            textBox3.Margin = new Padding(4, 4, 4, 4);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
-            textBox3.Size = new Size(126, 27);
+            textBox3.Size = new Size(156, 31);
             textBox3.TabIndex = 9;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(176, 103);
+            textBox2.Location = new Point(220, 129);
+            textBox2.Margin = new Padding(4, 4, 4, 4);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(126, 27);
+            textBox2.Size = new Size(156, 31);
             textBox2.TabIndex = 8;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(176, 70);
+            textBox1.Location = new Point(220, 88);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(126, 27);
+            textBox1.Size = new Size(156, 31);
             textBox1.TabIndex = 7;
             // 
             // label5
             // 
             label5.Font = new Font("Arial", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(6, 23);
+            label5.Location = new Point(8, 29);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(296, 38);
+            label5.Size = new Size(370, 48);
             label5.TabIndex = 4;
             label5.Text = "Datos del Cliente";
             // 
-            // label4
-            // 
-            label4.Location = new Point(6, 169);
-            label4.Name = "label4";
-            label4.Size = new Size(139, 23);
-            label4.TabIndex = 3;
-            label4.Text = "Dirección de retiro:";
-            // 
             // label3
             // 
-            label3.Location = new Point(6, 136);
+            label3.Location = new Point(8, 170);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(79, 23);
+            label3.Size = new Size(99, 29);
             label3.TabIndex = 2;
             label3.Text = "Teléfono:";
             // 
             // label2
             // 
-            label2.Location = new Point(6, 101);
+            label2.Location = new Point(8, 126);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(160, 26);
+            label2.Size = new Size(200, 32);
             label2.TabIndex = 1;
             label2.Text = "Nombre/Razón social:";
             // 
             // label1
             // 
-            label1.Location = new Point(6, 71);
+            label1.Location = new Point(8, 89);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(79, 26);
+            label1.Size = new Size(99, 32);
             label1.TabIndex = 0;
             label1.Text = "ID Cliente:";
             // 
@@ -165,153 +153,154 @@
             groupBox2.Controls.Add(button3);
             groupBox2.Controls.Add(textBox10);
             groupBox2.Controls.Add(label16);
-            groupBox2.Controls.Add(comboBox2);
             groupBox2.Controls.Add(textBox6);
             groupBox2.Controls.Add(textBox5);
-            groupBox2.Controls.Add(label11);
             groupBox2.Controls.Add(label10);
             groupBox2.Controls.Add(label9);
             groupBox2.Controls.Add(label8);
             groupBox2.Controls.Add(comboBox1);
             groupBox2.Controls.Add(label6);
-            groupBox2.Location = new Point(31, 358);
+            groupBox2.Location = new Point(39, 387);
+            groupBox2.Margin = new Padding(4, 4, 4, 4);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(504, 341);
+            groupBox2.Padding = new Padding(4, 4, 4, 4);
+            groupBox2.Size = new Size(630, 393);
             groupBox2.TabIndex = 2;
             groupBox2.TabStop = false;
             // 
             // button5
             // 
-            button5.Location = new Point(284, 230);
+            button5.Location = new Point(309, 240);
+            button5.Margin = new Padding(4, 4, 4, 4);
             button5.Name = "button5";
-            button5.Size = new Size(104, 60);
+            button5.Size = new Size(143, 78);
             button5.TabIndex = 25;
             button5.Text = "Cancelar";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(165, 230);
+            button4.Location = new Point(159, 240);
+            button4.Margin = new Padding(4, 4, 4, 4);
             button4.Name = "button4";
-            button4.Size = new Size(101, 60);
+            button4.Size = new Size(142, 78);
             button4.TabIndex = 24;
             button4.Text = "Imprimir guía";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(6, 228);
+            button3.Location = new Point(6, 240);
+            button3.Margin = new Padding(4, 4, 4, 4);
             button3.Name = "button3";
-            button3.Size = new Size(137, 62);
+            button3.Size = new Size(144, 78);
             button3.TabIndex = 23;
             button3.Text = "Confirmar imposición";
             button3.UseVisualStyleBackColor = true;
             // 
             // textBox10
             // 
-            textBox10.Location = new Point(129, 308);
+            textBox10.Location = new Point(159, 340);
+            textBox10.Margin = new Padding(4, 4, 4, 4);
             textBox10.Name = "textBox10";
             textBox10.ReadOnly = true;
-            textBox10.Size = new Size(126, 27);
+            textBox10.Size = new Size(156, 31);
             textBox10.TabIndex = 22;
             // 
             // label16
             // 
             label16.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label16.Location = new Point(6, 311);
+            label16.Location = new Point(6, 344);
+            label16.Margin = new Padding(4, 0, 4, 0);
             label16.Name = "label16";
-            label16.Size = new Size(127, 24);
+            label16.Size = new Size(159, 30);
             label16.TabIndex = 21;
             label16.Text = "N° de Guía:";
             // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(165, 188);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(126, 28);
-            comboBox2.TabIndex = 12;
-            // 
             // textBox6
             // 
-            textBox6.Location = new Point(165, 151);
+            textBox6.Location = new Point(206, 189);
+            textBox6.Margin = new Padding(4, 4, 4, 4);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(126, 27);
+            textBox6.Size = new Size(156, 31);
             textBox6.TabIndex = 11;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(165, 114);
+            textBox5.Location = new Point(206, 142);
+            textBox5.Margin = new Padding(4, 4, 4, 4);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(126, 27);
+            textBox5.Size = new Size(156, 31);
             textBox5.TabIndex = 10;
-            // 
-            // label11
-            // 
-            label11.Location = new Point(8, 188);
-            label11.Name = "label11";
-            label11.Size = new Size(137, 26);
-            label11.TabIndex = 9;
-            label11.Text = "Tipo de servicio:";
             // 
             // label10
             // 
-            label10.Location = new Point(8, 114);
+            label10.Location = new Point(10, 142);
+            label10.Margin = new Padding(4, 0, 4, 0);
             label10.Name = "label10";
-            label10.Size = new Size(137, 26);
+            label10.Size = new Size(171, 32);
             label10.TabIndex = 8;
             label10.Text = "Dirección de retiro:";
             // 
             // label9
             // 
-            label9.Location = new Point(8, 151);
+            label9.Location = new Point(10, 189);
+            label9.Margin = new Padding(4, 0, 4, 0);
             label9.Name = "label9";
-            label9.Size = new Size(77, 26);
+            label9.Size = new Size(188, 32);
             label9.TabIndex = 7;
-            label9.Text = "Destino:";
+            label9.Text = "Dirección de destino:";
+            label9.Click += label9_Click;
             // 
             // label8
             // 
-            label8.Location = new Point(8, 78);
+            label8.Location = new Point(10, 98);
+            label8.Margin = new Padding(4, 0, 4, 0);
             label8.Name = "label8";
-            label8.Size = new Size(97, 21);
+            label8.Size = new Size(121, 26);
             label8.TabIndex = 6;
             label8.Text = "Tipo de caja:";
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(165, 78);
+            comboBox1.Location = new Point(206, 98);
+            comboBox1.Margin = new Padding(4, 4, 4, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(126, 28);
+            comboBox1.Size = new Size(156, 33);
             comboBox1.TabIndex = 6;
             // 
             // label6
             // 
             label6.Font = new Font("Arial", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(8, 23);
+            label6.Location = new Point(10, 29);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(354, 36);
+            label6.Size = new Size(442, 45);
             label6.TabIndex = 5;
             label6.Text = "Datos de la encomienda";
             // 
             // label17
             // 
             label17.Font = new Font("Arial", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label17.Location = new Point(100, 27);
+            label17.Location = new Point(125, 34);
+            label17.Margin = new Padding(4, 0, 4, 0);
             label17.Name = "label17";
-            label17.Size = new Size(387, 31);
+            label17.Size = new Size(484, 39);
             label17.TabIndex = 5;
             label17.Text = "Imposición de Encomienda";
             // 
             // ImponerEncomienda
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(573, 719);
+            ClientSize = new Size(716, 802);
             Controls.Add(label17);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "ImponerEncomienda";
             Text = "ImponerEncomienda";
             groupBox1.ResumeLayout(false);
@@ -326,19 +315,15 @@
         private Button button1;
         private GroupBox groupBox1;
         private Label label5;
-        private Label label4;
         private Label label3;
         private Label label2;
         private Label label1;
         private GroupBox groupBox2;
-        private TextBox textBox4;
         private TextBox textBox3;
         private TextBox textBox2;
         private TextBox textBox1;
-        private ComboBox comboBox2;
         private TextBox textBox6;
         private TextBox textBox5;
-        private Label label11;
         private Label label10;
         private Label label9;
         private Label label8;
