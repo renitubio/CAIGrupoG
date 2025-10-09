ecb23baaea581829c336216fd9e6fe507ccdc891
AbrilAbad <164096598+AbrilAbad@users.noreply.github.com>
2025-10-08 21:19:10 -0300

    Ajuste de elemento cantidad en imposición

--- Imposicion/ImpCallCenter/ImposicionDeEncomiendaCallCenterForm.Designer.cs
+++ Imposicion/ImpCallCenter/ImposicionDeEncomiendaCallCenterForm.Designer.cs
@@ -39,7 +39,6 @@
             comboBox3 = new ComboBox();
             comboBox2 = new ComboBox();
             label3 = new Label();
-            textBox2 = new TextBox();
             label2 = new Label();
             label9 = new Label();
             label8 = new Label();
@@ -52,12 +51,15 @@
             label5 = new Label();
             button1 = new Button();
             label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
             groupBox2.SuspendLayout();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // groupBox2
             // 
+            groupBox2.Controls.Add(numericUpDown1);
             groupBox2.Controls.Add(textBox4);
             groupBox2.Controls.Add(label7);
             groupBox2.Controls.Add(textBox3);
@@ -68,173 +70,154 @@
             groupBox2.Controls.Add(comboBox3);
             groupBox2.Controls.Add(comboBox2);
             groupBox2.Controls.Add(label3);
-            groupBox2.Controls.Add(textBox2);
             groupBox2.Controls.Add(label2);
             groupBox2.Controls.Add(label9);
             groupBox2.Controls.Add(label8);
             groupBox2.Controls.Add(comboBox1);
             groupBox2.Controls.Add(label6);
-            groupBox2.Location = new Point(12, 118);
-            groupBox2.Margin = new Padding(3, 2, 3, 2);
+            groupBox2.Location = new Point(14, 157);
             groupBox2.Name = "groupBox2";
-            groupBox2.Padding = new Padding(3, 2, 3, 2);
-            groupBox2.Size = new Size(556, 318);
+            groupBox2.Size = new Size(635, 424);
             groupBox2.TabIndex = 7;
             groupBox2.TabStop = false;
             groupBox2.Enter += groupBox2_Enter;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(144, 277);
-            textBox4.Margin = new Padding(3, 2, 3, 2);
+            textBox4.Location = new Point(165, 369);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(110, 23);
+            textBox4.Size = new Size(125, 27);
             textBox4.TabIndex = 29;
             // 
             // label7
             // 
-            label7.Location = new Point(6, 284);
+            label7.Location = new Point(7, 379);
             label7.Name = "label7";
-            label7.Size = new Size(132, 16);
+            label7.Size = new Size(151, 21);
             label7.TabIndex = 28;
             label7.Text = "DNI autorizado a retirar:";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(352, 74);
-            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Location = new Point(402, 99);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(110, 23);
+            textBox3.Size = new Size(125, 27);
             textBox3.TabIndex = 27;
             // 
             // label4
             // 
-            label4.Location = new Point(273, 78);
+            label4.Location = new Point(312, 104);
             label4.Name = "label4";
-            label4.Size = new Size(132, 19);
+            label4.Size = new Size(151, 25);
             label4.TabIndex = 26;
             label4.Text = "Domicilio:";
             // 
             // button4
             // 
-            button4.Location = new Point(273, 168);
-            button4.Margin = new Padding(3, 2, 3, 2);
+            button4.Location = new Point(312, 224);
             button4.Name = "button4";
-            button4.Size = new Size(101, 25);
+            button4.Size = new Size(115, 33);
             button4.TabIndex = 25;
             button4.Text = "Quitar";
             button4.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(273, 135);
-            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Location = new Point(312, 180);
             button2.Name = "button2";
-            button2.Size = new Size(101, 25);
+            button2.Size = new Size(115, 33);
             button2.TabIndex = 24;
             button2.Text = "Añadir";
             button2.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
-            listView1.Location = new Point(6, 168);
+            listView1.Location = new Point(7, 224);
+            listView1.Margin = new Padding(3, 4, 3, 4);
             listView1.Name = "listView1";
-            listView1.Size = new Size(248, 97);
+            listView1.Size = new Size(283, 128);
             listView1.TabIndex = 19;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
             // comboBox3
             // 
             comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(143, 78);
-            comboBox3.Margin = new Padding(3, 2, 3, 2);
+            comboBox3.Location = new Point(163, 104);
             comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(110, 23);
+            comboBox3.Size = new Size(125, 28);
             comboBox3.TabIndex = 18;
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(143, 44);
-            comboBox2.Margin = new Padding(3, 2, 3, 2);
+            comboBox2.Location = new Point(163, 59);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(110, 23);
+            comboBox2.Size = new Size(125, 28);
             comboBox2.TabIndex = 17;
             // 
             // label3
             // 
-            label3.Location = new Point(7, 44);
+            label3.Location = new Point(8, 59);
             label3.Name = "label3";
-            label3.Size = new Size(132, 19);
+            label3.Size = new Size(151, 25);
             label3.TabIndex = 14;
             label3.Text = "Ciudad:";
             // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(144, 135);
-            textBox2.Margin = new Padding(3, 2, 3, 2);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(110, 23);
-            textBox2.TabIndex = 13;
-            // 
             // label2
             // 
-            label2.Location = new Point(7, 138);
+            label2.Location = new Point(8, 188);
             label2.Name = "label2";
-            label2.Size = new Size(85, 16);
+            label2.Size = new Size(97, 21);
             label2.TabIndex = 12;
             label2.Text = "Cantidad:";
             // 
             // label9
             // 
-            label9.Location = new Point(7, 78);
+            label9.Location = new Point(8, 104);
             label9.Name = "label9";
-            label9.Size = new Size(132, 19);
+            label9.Size = new Size(151, 25);
             label9.TabIndex = 7;
             label9.Text = "Opciones de entrega:";
             // 
             // label8
             // 
-            label8.Location = new Point(7, 112);
+            label8.Location = new Point(8, 149);
             label8.Name = "label8";
-            label8.Size = new Size(85, 16);
+            label8.Size = new Size(97, 21);
             label8.TabIndex = 6;
             label8.Text = "Tipo de caja:";
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(144, 108);
-            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Location = new Point(163, 144);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(110, 23);
+            comboBox1.Size = new Size(125, 28);
             comboBox1.TabIndex = 6;
             // 
             // label6
             // 
             label6.Font = new Font("Arial", 12F, FontStyle.Bold);
-            label6.Location = new Point(7, 17);
+            label6.Location = new Point(8, 23);
             label6.Name = "label6";
-            label6.Size = new Size(309, 27);
+            label6.Size = new Size(353, 36);
             label6.TabIndex = 5;
             label6.Text = "Datos de la encomienda";
             // 
             // button5
             // 
-            button5.Location = new Point(448, 440);
-            button5.Margin = new Padding(3, 2, 3, 2);
+            button5.Location = new Point(512, 587);
             button5.Name = "button5";
-            button5.Size = new Size(100, 25);
+            button5.Size = new Size(114, 33);
             button5.TabIndex = 25;
             button5.Text = "Cancelar";
             button5.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(341, 440);
-            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Location = new Point(390, 587);
             button3.Name = "button3";
-            button3.Size = new Size(101, 25);
+            button3.Size = new Size(115, 33);
             button3.TabIndex = 23;
             button3.Text = "Confirmar";
             button3.UseVisualStyleBackColor = true;
@@ -245,59 +228,62 @@
             groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(button1);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(12, 11);
-            groupBox1.Margin = new Padding(3, 2, 3, 2);
+            groupBox1.Location = new Point(14, 15);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(3, 2, 3, 2);
-            groupBox1.Size = new Size(556, 103);
+            groupBox1.Size = new Size(635, 137);
             groupBox1.TabIndex = 6;
             groupBox1.TabStop = false;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(49, 49);
-            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Location = new Point(56, 65);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(110, 23);
+            textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 7;
             // 
             // label5
             // 
             label5.Font = new Font("Arial", 12F, FontStyle.Bold);
-            label5.Location = new Point(6, 17);
+            label5.Location = new Point(7, 23);
             label5.Name = "label5";
-            label5.Size = new Size(226, 21);
+            label5.Size = new Size(258, 28);
             label5.TabIndex = 4;
             label5.Text = "Datos del Cliente";
             // 
             // button1
             // 
-            button1.Location = new Point(178, 44);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(203, 59);
             button1.Name = "button1";
-            button1.Size = new Size(118, 32);
+            button1.Size = new Size(135, 43);
             button1.TabIndex = 0;
             button1.Text = "Buscar Cliente";
             button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            label1.Location = new Point(6, 53);
+            label1.Location = new Point(7, 71);
             label1.Name = "label1";
-            label1.Size = new Size(69, 19);
+            label1.Size = new Size(79, 25);
             label1.TabIndex = 0;
             label1.Text = "CUIT:";
             // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(163, 182);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(125, 27);
+            numericUpDown1.TabIndex = 31;
+            // 
             // ImposicionDeEncomiendaCallCenterForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(580, 539);
+            ClientSize = new Size(663, 642);
             Controls.Add(button3);
             Controls.Add(button5);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Margin = new Padding(2);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "ImposicionDeEncomiendaCallCenterForm";
             Text = "Imposición de Encomienda - Call Center";
             Load += ImposicionCallCenterForm_Load;
@@ -305,6 +291,7 @@
             groupBox2.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
         }
 
@@ -322,7 +309,6 @@
         private Button button1;
         private Label label1;
         private Label label3;
-        private TextBox textBox2;
         private Label label2;
         private ComboBox comboBox2;
         private Label label4;
@@ -333,5 +319,6 @@
         private TextBox textBox4;
         private Label label7;
         private TextBox textBox3;
+        private NumericUpDown numericUpDown1;
     }
 }
