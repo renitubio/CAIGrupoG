21eec12003c5d0ecec591e67f0027cab7fd8a61b
Ivan Otero <Ivan Otero@DESKTOP-9OPP9JT>
2025-11-05 20:04:43 -0300

    Menu principal actualizacion

--- MenuPrincipal.Designer.cs
+++ MenuPrincipal.Designer.cs
@@ -33,18 +33,26 @@ partial class MenuPrincipal
         AgenciaCombo = new ComboBox();
         label1 = new Label();
         label2 = new Label();
-        ImposicionAgenciaBttn = new Button();
+        EmitirFacturaButton = new Button();
+        EntregaGuiaAgenciaButton = new Button();
+        EntregaGuiaCDButton = new Button();
+        ImpAgenciaButton = new Button();
+        ImpCallCenterButton = new Button();
+        PlayeroButton = new Button();
+        RendicionFleteroButton = new Button();
+        CostosVentasButton = new Button();
+        ImpCentroDistribucionButton = new Button();
         SuspendLayout();
         // 
         // ConsultaTrackingBttn
         // 
-        ConsultaTrackingBttn.Location = new Point(24, 110);
-        ConsultaTrackingBttn.Name = "ConsultaTrackingBttn";
-        ConsultaTrackingBttn.Size = new Size(167, 23);
-        ConsultaTrackingBttn.TabIndex = 0;
-        ConsultaTrackingBttn.Text = "Consulta Tracking";
-        ConsultaTrackingBttn.UseVisualStyleBackColor = true;
-        ConsultaTrackingBttn.Click += ConsultaTrackingBttn_Click;
+        ConsultaTrackingButton.Location = new Point(238, 208);
+        ConsultaTrackingButton.Name = "ConsultaTrackingButton";
+        ConsultaTrackingButton.Size = new Size(167, 23);
+        ConsultaTrackingButton.TabIndex = 0;
+        ConsultaTrackingButton.Text = "Consulta Tracking";
+        ConsultaTrackingButton.UseVisualStyleBackColor = true;
+        ConsultaTrackingButton.Click += ConsultaTrackingButton_Click;
         // 
         // CdCombo
         // 
@@ -76,28 +84,118 @@ partial class MenuPrincipal
         // label2
         // 
         label2.AutoSize = true;
-        label2.Location = new Point(48, 53);
+        label2.Location = new Point(30, 53);
         label2.Name = "label2";
         label2.Size = new Size(88, 15);
         label2.TabIndex = 4;
         label2.Text = "Agencia actual:";
         // 
-        // ImposicionAgenciaBttn
+        // EmitirFacturaButton
         // 
-        ImposicionAgenciaBttn.Location = new Point(223, 110);
-        ImposicionAgenciaBttn.Name = "ImposicionAgenciaBttn";
-        ImposicionAgenciaBttn.Size = new Size(167, 23);
-        ImposicionAgenciaBttn.TabIndex = 5;
-        ImposicionAgenciaBttn.Text = "Imposicion Agencia";
-        ImposicionAgenciaBttn.UseVisualStyleBackColor = true;
-        ImposicionAgenciaBttn.Click += ImposicionAgenciaBttn_Click;
+        EmitirFacturaButton.Location = new Point(238, 300);
+        EmitirFacturaButton.Name = "EmitirFacturaButton";
+        EmitirFacturaButton.Size = new Size(167, 23);
+        EmitirFacturaButton.TabIndex = 5;
+        EmitirFacturaButton.Text = "Emitir factura";
+        EmitirFacturaButton.UseVisualStyleBackColor = true;
+        EmitirFacturaButton.Click += EmitirFacturaButton_Click;
+        // 
+        // EntregaGuiaAgenciaButton
+        // 
+        EntregaGuiaAgenciaButton.Location = new Point(238, 154);
+        EntregaGuiaAgenciaButton.Name = "EntregaGuiaAgenciaButton";
+        EntregaGuiaAgenciaButton.Size = new Size(167, 23);
+        EntregaGuiaAgenciaButton.TabIndex = 6;
+        EntregaGuiaAgenciaButton.Text = "Entrega guia agencia";
+        EntregaGuiaAgenciaButton.UseVisualStyleBackColor = true;
+        EntregaGuiaAgenciaButton.Click += EntregaGuiaAgenciaButton_Click;
+        // 
+        // EntregaGuiaCDButton
+        // 
+        EntregaGuiaCDButton.Location = new Point(238, 110);
+        EntregaGuiaCDButton.Name = "EntregaGuiaCDButton";
+        EntregaGuiaCDButton.Size = new Size(167, 23);
+        EntregaGuiaCDButton.TabIndex = 7;
+        EntregaGuiaCDButton.Text = "Entrega guia CD";
+        EntregaGuiaCDButton.UseVisualStyleBackColor = true;
+        EntregaGuiaCDButton.Click += EntregaGuiaCDButton_Click;
+        // 
+        // ImpAgenciaButton
+        // 
+        ImpAgenciaButton.Location = new Point(24, 110);
+        ImpAgenciaButton.Name = "ImpAgenciaButton";
+        ImpAgenciaButton.Size = new Size(167, 23);
+        ImpAgenciaButton.TabIndex = 8;
+        ImpAgenciaButton.Text = "Imposicion agencia";
+        ImpAgenciaButton.UseVisualStyleBackColor = true;
+        ImpAgenciaButton.Click += ImpAgenciaButton_Click;
+        // 
+        // ImpCallCenterButton
+        // 
+        ImpCallCenterButton.Location = new Point(24, 157);
+        ImpCallCenterButton.Name = "ImpCallCenterButton";
+        ImpCallCenterButton.Size = new Size(167, 23);
+        ImpCallCenterButton.TabIndex = 9;
+        ImpCallCenterButton.Text = "Imposicion call center";
+        ImpCallCenterButton.UseVisualStyleBackColor = true;
+        ImpCallCenterButton.Click += ImpCallCenterButton_Click;
+        // 
+        // PlayeroButton
+        // 
+        PlayeroButton.Location = new Point(24, 252);
+        PlayeroButton.Name = "PlayeroButton";
+        PlayeroButton.Size = new Size(167, 23);
+        PlayeroButton.TabIndex = 10;
+        PlayeroButton.Text = "Playero";
+        PlayeroButton.UseVisualStyleBackColor = true;
+        PlayeroButton.Click += PlayeroButton_Click;
+        // 
+        // RendicionFleteroButton
+        // 
+        RendicionFleteroButton.Location = new Point(24, 300);
+        RendicionFleteroButton.Name = "RendicionFleteroButton";
+        RendicionFleteroButton.Size = new Size(167, 23);
+        RendicionFleteroButton.TabIndex = 11;
+        RendicionFleteroButton.Text = "Rendicion fletero";
+        RendicionFleteroButton.UseVisualStyleBackColor = true;
+        RendicionFleteroButton.Click += RendicionFleteroButton_Click;
+        // 
+        // CostosVentasButton
+        // 
+        CostosVentasButton.Location = new Point(238, 252);
+        CostosVentasButton.Name = "CostosVentasButton";
+        CostosVentasButton.Size = new Size(167, 23);
+        CostosVentasButton.TabIndex = 12;
+        CostosVentasButton.Text = "Costos vs ventas";
+        CostosVentasButton.UseMnemonic = false;
+        CostosVentasButton.UseVisualStyleBackColor = true;
+        CostosVentasButton.UseWaitCursor = true;
+        CostosVentasButton.Click += CostosVentasButton_Click;
+        // 
+        // ImpCentroDistribucionButton
+        // 
+        ImpCentroDistribucionButton.Location = new Point(24, 208);
+        ImpCentroDistribucionButton.Name = "ImpCentroDistribucionButton";
+        ImpCentroDistribucionButton.Size = new Size(167, 23);
+        ImpCentroDistribucionButton.TabIndex = 13;
+        ImpCentroDistribucionButton.Text = "Imposicion CD";
+        ImpCentroDistribucionButton.UseVisualStyleBackColor = true;
+        ImpCentroDistribucionButton.Click += ImpCentroDistribucionButton_Click;
         // 
         // MenuPrincipal
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
-        Controls.Add(ImposicionAgenciaBttn);
+        ClientSize = new Size(431, 362);
+        Controls.Add(ImpCentroDistribucionButton);
+        Controls.Add(CostosVentasButton);
+        Controls.Add(RendicionFleteroButton);
+        Controls.Add(PlayeroButton);
+        Controls.Add(ImpCallCenterButton);
+        Controls.Add(ImpAgenciaButton);
+        Controls.Add(EntregaGuiaCDButton);
+        Controls.Add(EntregaGuiaAgenciaButton);
+        Controls.Add(EmitirFacturaButton);
         Controls.Add(label2);
         Controls.Add(label1);
         Controls.Add(AgenciaCombo);
@@ -117,5 +215,13 @@ partial class MenuPrincipal
     private ComboBox AgenciaCombo;
     private Label label1;
     private Label label2;
-    private Button ImposicionAgenciaBttn;
+    private Button EmitirFacturaButton;
+    private Button EntregaGuiaAgenciaButton;
+    private Button EntregaGuiaCDButton;
+    private Button ImpAgenciaButton;
+    private Button ImpCallCenterButton;
+    private Button PlayeroButton;
+    private Button RendicionFleteroButton;
+    private Button CostosVentasButton;
+    private Button ImpCentroDistribucionButton;
 }
