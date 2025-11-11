58ad334777e02532d5f14b116935d837dce1004d
Renata <renatubio@gmail.com>
2025-11-11 01:26:53 -0300

    volví la clase del prototipo, peor todavía no se usa

---
+++ Playero/Guia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGrupoG.Playero
+{
+    public class Guia
+    {
+        public string NumeroGuia { get; set; }
+        public TipoPaquete TipoPaquete { get; set; }
+        public string CUIT { get; set; }
+        public string CDOrigen { get; set; }
+        public string CDDestino { get; set; }
+        public EstadoGuia Estado { get; set; }
+    }
+    // Enumeración para los estados de la guía
+    public enum EstadoGuia
+    {
+        AdmitidoCDOrigen,
+        EnTransito,
+        AdmitidoCDDestino
+    }
+
+    // Enumeración para los tipos de paquete
+    public enum TipoPaquete
+    {
+        S,
+        M,
+        L,
+        XL
+    }
+}
